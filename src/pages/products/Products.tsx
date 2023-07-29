fafff62d7ed586300001c1adf682ea867526f262
Rahul <rahulsingh3118.rs@gmail.com>
2023-07-30 02:19:49 +0530

    [feat]users and products section

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -1,9 +1,71 @@
+import { useState } from "react";
+import AddUser from "../../components/addNew/AddNew";
+import DataTable from "../../components/dataTable/DataTable";
 import "./products.scss";
+import { GridColDef } from "@mui/x-data-grid";
+import { products } from "../../data";
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 60 },
+  {
+    field: "img",
+    headerName: "Avatar",
+    width: 100,
+    renderCell: (params) => {
+      return <img src={params.row.img || "/noavatar.png"} alt="" />;
+    },
+  },
+  {
+    field: "title",
+    headerName: "Title",
+    width: 250,
+    type: "string",
+  },
+  {
+    field: "color",
+    headerName: "Color",
+    width: 150,
+    type: "string",
+  },
+  {
+    field: "price",
+    headerName: "Price",
+    width: 100,
+    type: "string",
+  },
+  {
+    field: "producer",
+    headerName: "Producer",
+    width: 150,
+    type: "string",
+  },
+  {
+    field: "createdAt",
+    headerName: "Created At",
+    width: 160,
+    type: "string",
+  },
+  {
+    field: "inStock",
+    headerName: "In Stock",
+    width: 100,
+    type: "boolean",
+  },
+];
 
 const Products = () => {
-  return (
-    <div className='products'>Products</div>
-  )
-}
+  const [open, setOpen] = useState(false);
 
-export default Products
+  return (
+    <div className="products">
+      <div className="info">
+        <h1>Products</h1>
+        <button onClick={() => setOpen(true)}>Add New Product</button>
+      </div>
+      <DataTable slug="products" columns={columns} rows={products} />
+      {open && <AddUser setOpen={setOpen} slug="product" columns={columns} />}
+    </div>
+  );
+};
+
+export default Products;
--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,7 +1,70 @@
+import { GridColDef } from "@mui/x-data-grid";
+import DataTable from "../../components/dataTable/DataTable";
 import "./users.scss";
+import { userRows } from "../../data";
+import { useState } from "react";
+import AddUser from "../../components/addNew/AddNew";
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 60 },
+  {
+    field: "img",
+    headerName: "Avatar",
+    width: 100,
+    renderCell: (params) => {
+      return <img src={params.row.img || "/noavatar.png"} alt="" />;
+    },
+  },
+  {
+    field: "firstName",
+    headerName: "First name",
+    width: 150,
+    type: "string",
+  },
+  {
+    field: "lastName",
+    headerName: "Last name",
+    width: 150,
+    type: "string",
+  },
+  {
+    field: "email",
+    headerName: "Email",
+    width: 200,
+    type: "string",
+  },
+  {
+    field: "phone",
+    headerName: "Phone",
+    width: 150,
+    type: "string",
+  },
+  {
+    field: "createdAt",
+    headerName: "Created At",
+    width: 150,
+    type: "string",
+  },
+  {
+    field: "verified",
+    headerName: "Verified",
+    width: 80,
+    type: "boolean",
+  },
+];
 
 const Users = () => {
-  return <div className="users">Users</div>;
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="users">
+      <div className="info">
+        <h1>Users</h1>
+        <button onClick={() => setOpen(true)}>Add New User</button>
+      </div>
+      <DataTable slug="users" columns={columns} rows={userRows} />
+      {open && <AddUser setOpen={setOpen} slug="user" columns={columns} />}
+    </div>
+  );
 };
 
 export default Users;
